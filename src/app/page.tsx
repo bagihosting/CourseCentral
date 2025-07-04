6d4415798da90b62c71665d3b65769238b0c0cac
uwais Alqorni <alqorniu552@gmail.com>
2025-07-04 01:14:20 +0000

    Optomasi seo dan kecepatan pada halaman utama agar saat website kita di

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import React, { useEffect, useState } from 'react';
@@ -147,7 +148,7 @@ function LandingPage() {
               </div>
             </div>
             <div className="relative aspect-video rounded-2xl overflow-hidden shadow-2xl mx-auto w-full max-w-lg">
-                <Image src="https://placehold.co/600x400.png" alt="Siswa sedang belajar kursus online bersertifikat" fill className="object-cover" data-ai-hint="online learning students" />
+                <Image src="https://placehold.co/600x400.png" alt="Siswa sedang belajar kursus online bersertifikat" fill className="object-cover" data-ai-hint="online learning students" priority />
             </div>
           </div>
         </section>
