16ef81cf1c790bc537ae480600703fec5021f557
uwais Alqorni <alqorniu552@gmail.com>
2025-07-09 03:57:21 +0000

    Apakah api token ini bisa di pakai untuk website lain yang beda server

---
+++ src/next.config.ts
@@ -0,0 +1,38 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+      return [
+          {
+              // Berlaku untuk semua rute API di bawah /api/
+              source: "/api/:path*",
+              headers: [
+                  { key: "Access-Control-Allow-Credentials", value: "true" },
+                  { key: "Access-Control-Allow-Origin", value: "*" }, // Ganti dengan domain spesifik Anda di produksi
+                  { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+                  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+              ]
+          }
+      ]
+  }
+};
+
+export default nextConfig;
