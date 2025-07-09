5ca6f5b98b83cd2b31b046bb580719e70e9fd14c
uwais Alqorni <alqorniu552@gmail.com>
2025-07-09 08:18:52 +0000

    terjadi error lagi solusi [INFO] Mengkonfigurasi database MariaDB...
    ma

--- src/install.sh
+++ src/install.sh
@@ -71,7 +71,7 @@ DB_ROOT_PASS=$(openssl rand -base64 16)
 
 # Menjalankan semua perintah keamanan dan setup dalam satu sesi menggunakan sudo.
 # Ini menggunakan autentikasi soket unix untuk pengguna root OS, yang merupakan metode default dan paling andal.
-mariadb --protocol=socket --batch <<-EOSQL
+mariadb --no-defaults --protocol=socket --batch <<-EOSQL
   -- Mengatur kata sandi untuk pengguna root MariaDB, membuatnya dapat diakses dengan kata sandi.
   ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
 
